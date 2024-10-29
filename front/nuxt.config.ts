0e511e199c429cc384d3c3f007c50165fb9c17f2
david <david@lexprogram.com>
2024-10-29 13:25:31 +0100

    Añadido vuetify

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,11 +1,30 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@pinia/nuxt"],
   runtimeConfig: {
     public: {
       baseURL: process.env.API_URL || "https://api.example.com/",
+    },
+  },
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    "@pinia/nuxt",
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
     },
   },
 });
