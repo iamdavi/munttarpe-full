72fe2082c806423465dffc88ca149b8f430ffd33
david <david@lexprogram.com>
2024-10-29 08:11:48 +0100

    Leida cookie de token al refrescar la página

--- front/middleware/auth.global.ts
+++ front/middleware/auth.global.ts
@@ -1,10 +1,13 @@
 import { useAuthStore } from "@/store/auth";
 
 export default defineNuxtRouteMiddleware((to, from) => {
-  const authStore = useAuthStore();
-  const isAuthenticated = authStore.authenticated;
+  const { authenticated } = storeToRefs(useAuthStore());
+  const token = useCookie("token");
+  if (token.value) {
+    authenticated.value = true; // update the state to authenticated
+  }
   const publicRoutes = ["/login", "/register", "/"];
-  if (!isAuthenticated && !publicRoutes.includes(to.path)) {
+  if (!authenticated && !publicRoutes.includes(to.path)) {
     console.log("401 Unautorized");
     return navigateTo("/login");
   }
